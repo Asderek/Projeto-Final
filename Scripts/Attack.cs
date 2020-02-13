4404b05cea757e829d535481238735ac53e9d9f2
Asderek <pinheiro.lucasn@gmail.com>
2020-02-13 16:37:43 -0300

    Update Attack.cs

--- Scripts/Attack.cs
+++ Scripts/Attack.cs
@@ -6,6 +6,18 @@ public class Attack : MonoBehaviour {
 
     public GameObject owner;
 
+    /******************************************
+	* 
+	* private void OnTriggerEnter2D(Collider2D collision)
+	*		If the attack colliders with a gameObject with the tag enemy,
+    *       signal the gameManager to handle the interaction.
+	*
+	* Parameters
+	*       Collider2D collision - Default unity parameter
+    *
+	* Return
+	*
+	* ***************************************/
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
